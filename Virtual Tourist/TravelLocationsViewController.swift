d19d60403a0be3484606572ff0589b56667a238c
Raghav Mangrola <raghav.mangrola@gmail.com>
2016-06-04 11:52:37 -0700

    TravelLocationsVC basic setup

---
+++ Virtual Tourist/TravelLocationsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TravelLocationsViewController.swift
+//  Virtual Tourist
+//
+//  Created by Raghav Mangrola on 6/4/16.
+//  Copyright © 2016 Raghav Mangrola. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsViewController: UIViewController {
+
+  @IBOutlet weak var mapView: MKMapView!
+  
+  @IBAction func editButtonPressed(sender: AnyObject) {
+    
+  }
+}
+
