fd79287f0d9e53cf4d0cd267f385091a139eb687
Raghav Mangrola <raghav.mangrola@gmail.com>
2016-06-09 10:15:35 -0700

    Drop pins with coordinate data

--- Virtual Tourist/TravelLocationsViewController.swift
+++ Virtual Tourist/TravelLocationsViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 import MapKit
 
-class TravelLocationsViewController: UIViewController {
-
+class TravelLocationsViewController: UIViewController, UIGestureRecognizerDelegate, MKMapViewDelegate {
+  
   @IBOutlet weak var mapView: MKMapView!
   
   @IBAction func editButtonPressed(sender: AnyObject) {
     
   }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    mapView.delegate = self
+    let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(TravelLocationsViewController.addPinToMap(_:)))
+    lpgr.delegate = self
+    self.mapView.addGestureRecognizer(lpgr)
+  }
+  
+  func addPinToMap(gestureRecognizer: UILongPressGestureRecognizer) {
+    if gestureRecognizer.state == UIGestureRecognizerState.Began {
+      let point = gestureRecognizer.locationInView(mapView)
+      let coordinate = mapView.convertPoint(point, toCoordinateFromView: mapView)
+      
+      let annotation = MKPointAnnotation()
+      annotation.coordinate = coordinate
+      mapView.addAnnotation(annotation)
+    
+    }
+  }
+  
+  
+  
 }
 
+// MARK: MKMapViewDelegate
+
+extension TravelLocationsViewController {
+  func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+    let reuseId = "pin"
+    
+    var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+    pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+    pinView?.animatesDrop = true
+    
+    return pinView
+  }
+}
