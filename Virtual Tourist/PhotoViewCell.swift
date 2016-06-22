6ce5e33d5c72f44ef8e774645de3602bb34dac98
Raghav Mangrola <raghav.mangrola@gmail.com>
2016-06-22 09:48:57 -0700

    CoreDataStack Fix + UIActivityIndicator in Cell

--- Virtual Tourist/PhotoViewCell.swift
+++ Virtual Tourist/PhotoViewCell.swift
@@ -12,6 +12,7 @@ import UIKit
 class PhotoViewCell: UICollectionViewCell {
   
   @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
   
   var taskToCancelIfCellIsReused: NSURLSessionTask? {
     didSet {
